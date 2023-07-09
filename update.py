7c96f12f9215c33b11749315bf28239a3be4b3af
haru-kiku <haru11250321@gmail.com>
2023-07-09 11:18:18 +0900

    display_all_message

---
+++ update.py
@@ -0,0 +1,15 @@
+from db_config import Message
+from read_all import display_all_message
+
+
+
+
+def update_message():
+    msg = Message.get_by_id(3)
+    msg.user = "Tom Ford"
+    msg.save()
+
+
+if __name__ == "__main__":
+    display_all_message()
+    update_message()
